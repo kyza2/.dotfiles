df2d6d386866e33e5c63c352e4d15423fe675a9e
Evan Smith <evan.leigh@outlook.com>
2023-02-27 10:21:13 -0500

    change colors with `pywal`

--- .config/polybar/scripts/clock.sh
+++ .config/polybar/scripts/clock.sh
@@ -3,20 +3,20 @@
 hour=$(date +%I)
 
 case $hour in
-    "01") icon="󱐿" ;;
-    "02") icon="󱑀" ;;
-    "03") icon="󱑁" ;;
-    "04") icon="󱑂" ;;
-    "05") icon="󱑃" ;;
-    "06") icon="󱑄" ;;
-    "07") icon="󱑅" ;;
-    "08") icon="󱑆" ;;
-    "09") icon="󱑇" ;;
-    "10") icon="󱑈" ;;
-    "11") icon="󱑉" ;;
-    "12") icon="󱑊" ;;
+    "01") icon="󱐿 " ;;
+    "02") icon="󱑀 " ;;
+    "03") icon="󱑁 " ;;
+    "04") icon="󱑂 " ;;
+    "05") icon="󱑃 " ;;
+    "06") icon="󱑄 " ;;
+    "07") icon="󱑅 " ;;
+    "08") icon="󱑆 " ;;
+    "09") icon="󱑇 " ;;
+    "10") icon="󱑈 " ;;
+    "11") icon="󱑉 " ;;
+    "12") icon="󱑊 " ;;
 esac
 
-time=$(date +"$icon")
+time=$(date +"%I:%M %P")
 
 echo "$time"
