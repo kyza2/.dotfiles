c3314e55d47791d305e904861611b1657788a20c
Evan Smith <evan.leigh@outlook.com>
2023-08-01 04:12:21 -0400

    changes

--- .config/ranger/commands.py
+++
@@ -1,8 +0,0 @@
-from ranger.api.commands import Command
-
-class set_wallpaper(Command):
-    """
-    Custom function to change theme using pywal and set center aligned theme using feh.
-    """
-    def execute(self):
-        self.fm.execute_console('shell feh --bg-fill %f');
